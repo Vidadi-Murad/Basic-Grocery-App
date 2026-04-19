674218a322e2ca7d672a36eb42011c811795e7f9
VidadiMurad <vidad0murad@gmail.com>
2026-04-19 15:37:52 +0400

    First Commit

---
+++ App.java
@@ -0,0 +1,19 @@
+// Basic Grocery App
+
+// Libraries
+import java.util.Scanner;
+import java.lang.invoke.LambdaMetafactory;
+import java.util.Locale;
+
+public class App {
+    public static void main(String[] args){
+     
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US); 
+
+        
+        
+
+        scanner.close();
+    }
+}
